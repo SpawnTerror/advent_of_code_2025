99f160c61b625a641924710e0eff3e52dfed20ea
SpawnTerror <39906114+SpawnTerror@users.noreply.github.com>
2025-12-06 22:42:36 +0000

    Day 2 Part 1 Completed

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -1,10 +1,43 @@
 #!/usr/bin/python3
 
-def day_1_part_1():
-    with open('day_2/sample.txt', 'r') as f:
-        data = [line.strip().split(',') for line in f]
+def day_2_part_1():
 
-    print(data)
+    ranges = []
+    invalid_ids_total = 0
+
+    with open('day_2/input.txt', 'r') as f:
+
+        for line in f:
+            line = line.strip().split(',')
+
+            for entry in line:
+
+                if not entry:
+                    continue
+                
+                range_start, range_end = entry.split('-')
+                ranges.append([int(range_start), int(range_end)])
+
+    for r in ranges:
+             
+        for number in range(r[0], r[1]+1):
+
+            number_length = len(str(number))
+            number_string = str(number)
+
+            split_value = number_length // 2
+
+            part_1 = number_string[:split_value]
+            part_2 = number_string[split_value:]
+
+            if part_1 == part_2:
+                invalid_ids_total = invalid_ids_total + number
+        
+    print(f'Invalid IDs total is {invalid_ids_total}.')
+
+def day_2_part_2():
+
+    pass
 
 if __name__ == '__main__':
-    day_1_part_1()
+    day_2_part_1()
